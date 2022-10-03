3f4d07dd25d67799d6d118c5cd49f3385f5a727c
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-03 12:39:38 +0000

    Wykonanie Karty Pracy1 (7 zadań)

---
+++ KartaPracy1.py
@@ -0,0 +1,31 @@
+# # Zad 1
+# a = int(input())
+# b = int(input())
+# print(a**2 + b**2)
+
+# Zad 2
+# a = int(input())
+# b = int(input())
+# print((a + b)**2)
+
+# Zad 3
+# a = int(input())
+# b = int(input())
+# print((a-b)**3)
+
+# Zad 4
+# a, b, c = map(int, input().split())
+# print(a*b*c)
+
+#Zad 5
+# a = int(input())
+# b = int(input())
+# print(2*(a+b)/5)
+
+# Zad 6
+# brutto = float(input())
+# print(brutto / 1.23)
+
+# Zad 7
+# a, b = int(input()), int(input())
+# print(a % b)
