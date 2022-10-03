8e84cd2180e635d9158516c7595c5faa7b3136ed
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-03 13:02:29 +0000

    Dodano plik KartaPracy2.py (zadania 1-7)

---
+++ KartaPracy2.py
@@ -0,0 +1,50 @@
+# Zad 1
+# a = int(input())
+# if a % 3 == 0:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# Zad 2
+# a = int(input())
+# if a >= 100 and a < 1000 and a % 17 == 0:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+#Zad 3
+# wiek = int(input("Ile masz lat?\n"))
+# if wiek >= 18:
+#     print("TAK, jesteś pełnoletni")
+# else:
+#     print("NIE")
+
+# Zad 4
+# LIMIT = 20
+# waga = int(input("Ile ważysz ciężarówko? "))
+# if waga <= LIMIT:
+#     print("Możesz wjechać")
+# else:
+#     print("Lepiej nie wjeżdżaj")
+
+#Zad 5
+# a, b, c = map(int, input().split())
+# if (a < c and b > c) or (b < c and a > c):
+#     print("TAK, SPEŁNIA WARUNKI")
+# else:
+#     print("NIE SPEŁNIA")
+
+#Zad 6
+# a = int(input("Podaj dowolną liczbę: "))
+# p = int(input("Podaj liczbę pierwszą: "))
+# if ((a**p - a) % p == 0):
+#     print("TAK, spełnia MTF")
+# else:
+#     print("NIE, nie spełnia MTF")
+
+# Zad 7
+# p, k, s = map(int, input().split())
+# if s*3 >= (k-p):
+#     print("TAK, płaz da radę")
+# else:
+#     print("NIE, żaba nie dała rady.")
