dfcafad0db04cee80a4d85e6eaa6047af90be511
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-19 13:09:37 +0000

    Wykonanie KartaPracy3a (Zad 1-2)

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -22,37 +22,60 @@
 #  **
 #   *
 
-n = int(input())
-for i in range(n):
-    for j in range(i+1):
-        print("*", end="")
-    print()
+# n = int(input())
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*", end="")
+#     print()
     
-print()
+# print()
+# print()
+
+# for i in range(n):
+#     for j in range(n-i):
+#         print("*", end="")
+#     print()
+
+# print()
+# print()
+
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print(" ", end="")
+#     for k in range(n-i-1, n):
+#         print("*", end="")
+#     print()
+
+
+# print()
+# print()
+
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print("*", end="")
+#     for k in range(n-i-1, n):
+#         print("", end="")
+#     print()
+
+# Zad 1
+
+# n = int(input())
+# for i in range(n):
+#     print("*-|", end="")
+# print()
+
+# Zad 2 - PRE
+
+# print("Bartosz" + 5)
+# print("Bartosz" * 5)
+# print("*"*3)
+
+n = int(input())
+for i in range(1,n+1):
+    print("*"*i, end="")
+    if i % 2:
+        print("||", end="")
+    else:
+        print("--", end="")
 print()
 
-for i in range(n):
-    for j in range(n-i):
-        print("*", end="")
-    print()
-
-print()
-print()
-
-for i in range(n):
-    for j in range(n-i-1):
-        print(" ", end="")
-    for k in range(n-i-1, n):
-        print("*", end="")
-    print()
-
-
-print()
-print()
-
-for i in range(n):
-    for j in range(n-i-1):
-        print("*", end="")
-    for k in range(n-i-1, n):
-        print("", end="")
-    print()
