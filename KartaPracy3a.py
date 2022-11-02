7c55e226902d6b086dfd9e69cac27c961856e913
PanInf <77333045+PanInf@users.noreply.github.com>
2022-11-02 14:19:31 +0000

    Zmiany w KP3a

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -99,19 +99,41 @@
 
 # Zad 5
 
+# n = int(input())
+
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         if j == n//2 + 1:
+#             print("*", end="")
+#         elif i == n//2 + 1:
+#             print("-", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+# Zad 6
+
+# *****
+# *****
+# *****
+# *****
+# *****
+# punkty (i, j)
+# (1, 1) (1, 2) (1, 3) (1, 4) (1, 5)
+# (2, 1) (2, 2) (2, 3) (2, 4) (2, 5)
+# (3, 1) (3, 2) (3, 3) (3, 4) (3, 5)
+# (4, 1) (4, 2) (4, 3) (4, 4) (4, 5)
+# (5, 1) (5, 2) (5, 3) (5, 4) (5, 5)
 
 n = int(input())
-
-
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if j == n//2 + 1:
+for i in range(n):
+    for j in range(n):
+        if i + j == 6:
             print("*", end="")
-        elif i == n//2 + 1:
-            print("-", end="")
         else:
-            print(" ", end="")
+            print("-", end="")
     print()
+
 
 # Zad 7
 
