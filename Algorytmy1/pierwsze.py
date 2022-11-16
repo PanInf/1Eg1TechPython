d86f1bb7e3342d80513217692f3cfeba231be1dd
PanInf <77333045+PanInf@users.noreply.github.com>
2022-11-16 13:50:00 +0000

    Dodanie zadań pierwsze.py

---
+++ Algorytmy1/pierwsze.py
@@ -0,0 +1,11 @@
+# 1. Algorytm sprawdzający czy liczba jest pierwsza
+# liczba pierwsza dzieli sie tylko przez 1 i samą siebie
+# 2, 3, 5, 7, 11, 13, 17, 19 itd...
+# liczba x ma swój dzielnik (o ile go ma) w przedziale [2, sqrt(x)]
+
+
+# 2. Generator liczb pierwszych - liczby pierwsze w przedziale [p, q]
+
+
+
+# 3. Generator liczb pierwszych - początkowe k liczb pierwszych
--- KartyPracy/KartaPracy3a.py
+++ KartyPracy/KartaPracy3a.py
@@ -99,14 +99,14 @@
 
 # Zad 4
 
-n = 11
-for i in range(n):
-    for j in range(n):
-        if i-j==n//2 or j-i==n//2 or i+j==n//2 or i+j==3*(n//2):
-            print("*", end="")
-        else:
-            print(" ", end="")
-    print()
+# n = 11
+# for i in range(n):
+#     for j in range(n):
+#         if i-j==n//2 or j-i==n//2 or i+j==n//2 or i+j==3*(n//2):
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print()
 
 
 
