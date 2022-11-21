2811c5c0593577c1695fd8796d0162a6325abae3
PanInf <77333045+PanInf@users.noreply.github.com>
2022-11-21 14:10:04 +0000

    generator liczb pierwszych

--- Algorytmy1/pierwsze.py
+++ Algorytmy1/pierwsze.py
@@ -4,16 +4,39 @@
 # liczba x ma swój dzielnik (o ile go ma) w przedziale [2, sqrt(x)]
 # dzielnik właściwy - dzielnik liczby oprócz 1 i niej samej
 
-n = int(input())
-for i in range(2,int(n**0.5)+1):
-    if n % i == 0:
-        print("Liczba nie jest pierwsza")
-        exit()
-print("Liczba jest pierwsza")
+# n = int(input())
+# for i in range(2,int(n**0.5)+1):
+#     if n % i == 0:
+#         print("Liczba nie jest pierwsza")
+#         exit()
+# print("Liczba jest pierwsza")
 
 # 2. Generator liczb pierwszych - liczby pierwsze w przedziale [p, q]
 
-# [10;15] -> 11 i 13
-# [20;22] -> brak liczb pierwszych
+# p, q = map(int, input().split())
+# for i in range(p, q+1):
+#     flaga = True
+#     for j in range(2, int(i**0.5)+1):
+#         if i % j == 0:
+#             flaga = False
+#             break
+#     if flaga:
+#         print(i, end=" ")
+
+
 
 # 3. Generator liczb pierwszych - początkowe k liczb pierwszych
+
+# n = int(input("Podaj ile chcesz liczb pierwszych: "))
+
+# x = 2
+# while n > 0:
+#     flaga = True
+#     for i in range(2, x):
+#         if x % i == 0:
+#             flaga = False
+#             break
+#     if flaga:
+#         print(x, end=" ")
+#         n = n - 1
+#     x = x + 1
