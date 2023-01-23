8fcb261ed57384a95eb06e7b6624dbe1e6c25f4e
PanInf <77333045+PanInf@users.noreply.github.com>
2023-01-23 12:40:40 +0000

    Create listy2.py

---
+++ Listy/listy2.py
@@ -0,0 +1,16 @@
+//Wygeneruj tablicę n losowych liczb:
+from random import randint
+L = [for i in range(20): randint(1,21)]
+
+
+
+1. Znajdź największą liczbę w tablicy
+2. Znajdź najmniejszą liczbę w tablicy
+3. Podaj ile razy występuje najwieksza liczba w tablicy
+4. Podaj ile razy występuje najmniejsza liczba w tablicy
+5. Podaj rozpiętość tablicy (różnica max - min)
+6. Podaj sumę liczb w tablicy
+7. Podaj średnią wartość liczb w tablicy
+8. Których liczb jest więcej w tablicy: parzystych czy nieparzystych?
+9. Ile w tablicy jest liczb pierwszych?
+10. Podaj v-ce max i v-ce min liczb tablicy
