13b35700e326b960660a3a4ba80ae18cfa8c1aec
PanInf <77333045+PanInf@users.noreply.github.com>
2023-03-01 14:14:40 +0000

    Dodanie napisów

---
+++ Napisy/nap1.py
@@ -0,0 +1,41 @@
+# napisy jako prawie listy
+
+# s = input()
+# print(s)
+
+# for x in s:
+#     print(x)
+    
+# for i in range(len(s)):
+#     print(s[i])
+
+# napis a lista
+
+# L = [5,2,8,4]
+# print(s, L)
+# L.sort()
+# print(s, L)
+# s.sort() <- błąd, nie ma takiego czegoś
+# print(s, L)
+
+# konwersja napis <-> lista (list(), join())
+
+# s = input()
+# L = list(s)
+# print(s, L)
+# L.sort()
+# print(s, L)
+# s = "".join(L)
+# print(s, L)
+
+# sprawdz czy dane słowo jest palindromem
+
+s = input()
+L = list(s)
+R = list(s)  # R = L.copy()
+R.reverse()
+if L == R:
+    print("TAK")
+else:
+    print("NIE")
+    
